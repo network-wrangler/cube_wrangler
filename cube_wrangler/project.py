cf7c79b5d4366e75e6cadbb7a78fb03604ca0b08
Sijia Wang <wangsijia0628@gmail.com>
2025-07-14 18:09:02 -0400

    allow base network to not have required keys

--- cube_wrangler/project.py
+++ cube_wrangler/project.py
@@ -640,6 +640,41 @@ class Project(object):
             if len(cube_add_df) == 0:
                 WranglerLogger.debug("No link additions processed")
                 return {}
+            
+            # The following section of code is to ensure that 
+            # the cube_add_df has all the required columns
+            # that are expected in the standard project card schema, 
+            # even when the base network and log files are missing them
+            # so that we can still create a project card for a network 
+            # that is not using the Network Wrangler standard schema.
+            # users can set such parameters in their implementation.
+            if "no_model_link_id_in_cube" in self.parameters.__dict__.keys():
+                if self.parameters.no_model_link_id_in_cube:
+                    cube_add_df["model_link_id"] = range(
+                        self.base_roadway_network.links_df.model_link_id.max() + 1,
+                        self.base_roadway_network.links_df.model_link_id.max() + 1 + len(cube_add_df)
+                    )
+            
+            if "no_lanes_in_cube" in self.parameters.__dict__.keys():
+                if self.parameters.no_lanes_in_cube:
+                    cube_add_df["lanes"] = 0
+            
+            if "no_drive_access_in_cube" in self.parameters.__dict__.keys():
+                if self.parameters.no_drive_access_in_cube:
+                    cube_add_df["drive_access"] = 1
+            
+            if "no_walk_access_in_cube" in self.parameters.__dict__.keys():
+                if self.parameters.no_walk_access_in_cube:
+                    cube_add_df["walk_access"] = 1
+            
+            if "no_bike_access_in_cube" in self.parameters.__dict__.keys():
+                if self.parameters.no_bike_access_in_cube:
+                    cube_add_df["bike_access"] = 1
+
+            if "no_name_in_cube" in self.parameters.__dict__.keys():
+                if self.parameters.no_name_in_cube:
+                    cube_add_df["name"] = "unknown"
+            # end of section to ensure required columns
 
             if limit_variables_to_existing_network:
                 add_col = [
@@ -856,7 +891,7 @@ class Project(object):
                         set_value = float(set_value)
                         exist_value = float(base_row[c])
                     else:
-                        None
+                        exist_value = base_row[c]
                     if isinstance(base_row[c], bool):
                         set_value = bool(int(set_value))
 
